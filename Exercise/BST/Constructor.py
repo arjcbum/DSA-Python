b705db027e6b47fd344d1d3fcd6b61359e019482
Abhishek Raj <abhirajstudy@gmail.com>
2023-12-19 22:37:11 +0530

    BST Exercise

---
+++ Exercise/BST/Constructor.py
@@ -0,0 +1,24 @@
+# class Node:
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+# class BinarySearchTree:
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+my_tree = BinarySearchTree()
+
+print("Root:", my_tree.root)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Root: None
+
+"""
