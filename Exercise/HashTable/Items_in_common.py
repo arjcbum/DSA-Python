6f65888c791e0890c77515a70acc66d8b50b229b
Abhishek Raj <abhirajstudy@gmail.com>
2024-02-03 12:05:29 +0530

    Another method

--- Exercise/HashTable/Items_in_common.py
+++ Exercise/HashTable/Items_in_common.py
@@ -4,16 +4,20 @@
 #                                    #
 #                                    #
 ######################################
+# def item_in_common(list1, list2):
+#     n_list = {}
+#     for i in list1:
+#         n_list[i] = 1
+#     for i in list2:
+#         if i in n_list.keys():
+#             return True
+#     return False
+
 def item_in_common(list1, list2):
-    n_list = {}
     for i in list1:
-        n_list[i] = 1
-    for i in list2:
-        if i in n_list.keys():
+        if i in list2:
             return True
     return False
-
-
 
 list1 = [1,3,5]
 list2 = [2,4,5]
